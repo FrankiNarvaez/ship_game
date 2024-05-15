8ede889c526fa2f3b19862ded1d13ec47ce463aa
Franki Jhan Carlos Narvaez Muñoz <frannki03@gmail.com>
2024-05-15 12:05:33 -0500

    First commit

--- Nave_Espacial/obj/Debug/net8.0/Nave_Espacial.AssemblyInfo.cs
+++ Nave_Espacial/obj/Debug/net8.0/Nave_Espacial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nave_Espacial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e08f34b40a8d957161b4104c2fc3bc98d362eb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nave_Espacial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nave_Espacial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
